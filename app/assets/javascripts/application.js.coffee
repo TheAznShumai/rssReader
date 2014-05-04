89a539de71bc18c78292d516d6117412d48e028c
Johnny Loi <theaznshumai@gmail.com>
2014-05-03 20:48:22 -0700

    init commit

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,10 @@
+#= require jquery
+#= require handlebars
+#= require ember
+#= require ember-data
+#= require_self
+#= require rss_reader
+
+# for more details see: http://emberjs.com/guides/application/
+window.RssReader = Ember.Application.create()
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RssReader::Application.config.secret_key_base = 'e9914c14c22caa232ca289f5a9089b85cce4fa41cfa16749ae49cf4181a6d855f234da4b5ef3e8c3507a35339288c21c9f81f0a49f62d52265b404db3a886766'
